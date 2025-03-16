d970ba061b531d55f7c7813f6a9219c1a70d963e
hennickc <hennickc@amazon.com>
2025-03-16 12:18:36 -0700

    Bug fix

--- src/board/monotonic_hash.rs
+++ src/board/monotonic_hash.rs
@@ -71,8 +71,8 @@ impl Board {
         ];
         const TOTAL_COMBINATIONS: u64 = CHOOSE_OF_48.iter().sum();
 
-        let mut white_pawns_left = white_pawns.count_ones() as u64;
-        let mut black_pawns_left = black_pawns.count_ones() as u64;
+        let mut white_pawns_left = white_pawns.count_ones() as usize;
+        let mut black_pawns_left = black_pawns.count_ones() as usize;
         let mut white_index = 0;
         let mut black_index = 0;
         for captured_or_promoted in (1..=8).rev() {
@@ -85,11 +85,11 @@ impl Board {
         }
         for square in 8..=55 {
             if white_pawns & BB_SQUARES[64 - square] != 0 {
-                white_index += count_combinations((square - 8) as u64, white_pawns_left);
+                white_index += count_combinations((square - 8) as u64, white_pawns_left as u64);
                 white_pawns_left -= 1;
             }
             if black_pawns & BB_SQUARES[square] != 0 {
-                black_index +=  count_combinations((56 - square) as u64, black_pawns_left);
+                black_index +=  count_combinations((56 - square) as u64, black_pawns_left as u64);
                 black_pawns_left -= 1;
             }
         }
